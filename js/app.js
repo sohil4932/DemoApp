7ab87c2dce2948ded33548cf2b58b37169fd0025
Sohil Patel <sohilpatel4932@gmail.com>
2015-07-02 20:46:04 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+	$('#cn-button').removeClass('ui-btn ui-shadow');
+
+	    $('#home .ui-content').css('height',$(window).height());
+
+	    var gallery = new Swiper('.gallery-top', {
+	        autoplay: 5000,
+	        speed: 1000,
+	        spaceBetween: 10
+		    });
+
+	    // Gallery Click Event
+	    $('.gallery').click(function(){
+	    	
+	    });
+});
