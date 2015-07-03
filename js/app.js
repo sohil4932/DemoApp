36b71108e0d5e4a051aa93b08fb07144b3250ebc
Sohil Patel <sohilpatel4932@gmail.com>
2015-07-04 00:36:54 +0530

    ADDED: new files

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
 
 	    $('#home .ui-content').css('height',$(window).height());
 
-	    var gallery = new Swiper('.gallery-top', {
+	    var galleryHome = new Swiper('.gallery-top', {
 	        autoplay: 5000,
 	        speed: 1000,
 	        spaceBetween: 10
@@ -14,7 +14,39 @@ jQuery(document).ready(function($) {
 	    	
 	    });
 
-	    //photoswiper
-	    
+	    $('.floorplan').click(function(){
+	    	
+	    });
 
+	    
+	    $('map').imageMapResize();
+
+	    $('#floor .floormap').height($(window).height());
+
+	    //photoswiper
+  		$("map[name=map] area").on('click', function () {
+	   				var pswpElement = document.querySelectorAll('.pswp')[0];
+
+	    // build items array
+	    var items = [
+	        {
+	            src: $(this).attr('data'),
+	            w: 1024,
+	            h: 768
+	        }
+	    ];
+	    
+	    // define options (if needed)
+	    var options = {
+	        // history & focus options are disabled on CodePen        
+	        history: false,
+	        focus: false,
+	        maxSpreadZoom:2,
+	        showAnimationDuration: 0,
+	        hideAnimationDuration: 0 
+	    };
+	    
+	    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+	    		gallery.init();
+		   });
 })
