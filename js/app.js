8baacb77443f0902de2c917e7b7a145e1a5e6b87
Kishan Jobanputra <kishan_jobanputra@yahoo.in>
2015-07-04 02:20:17 -0700

    added location.reload

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($) {
 
 	    $('#home .ui-content').css('height',$(window).height());
 
-	    var galleryHome = new Swiper('.gallery-top', {
+	    var homeGallery = new Swiper('.gallery-top', {
 	        autoplay: 5000,
 	        speed: 1000,
 	        spaceBetween: 10
@@ -48,30 +48,4 @@ jQuery(document).ready(function($) {
     		alert('asdd');
     	});
 
-	    //photoswiper
-  		$("map[name=map] area").on('click', function () {
-	   				var pswpElement = document.querySelectorAll('.pswp')[0];
-
-	    // build items array
-	    var items = [
-	        {
-	            src: $(this).attr('data'),
-	            w: 1024,
-	            h: 768
-	        }
-	    ];
-	    
-	    // define options (if needed)
-	    var options = {
-	        // history & focus options are disabled on CodePen        
-	        history: false,
-	        focus: false,
-	        maxSpreadZoom:2,
-	        showAnimationDuration: 0,
-	        hideAnimationDuration: 0 
-	    };
-	    
-	    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-	    		gallery.init();
-		   });
 })
